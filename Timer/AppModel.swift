6641382a3aaae2e087807d8de4c2799e1dc7f6f3
Kosei Himeno <k.himeno314@gmail.com>
2016-07-17 16:18:59 +0900

    Modelの作成

--- Timer/AppModel.swift
+++ Timer/AppModel.swift
@@ -9,11 +9,45 @@
 import Foundation
 import RealmSwift
 
-class AppModel: Object {
+class TimeTable: Object {
+    // TimeTable と TimeTask は1対多のリレーション
+    dynamic var id: Int = 0
+    dynamic var title: String = ""
+    dynamic var create_at = NSDate()
+    dynamic var update_at = NSDate()
+    let tasks = List<Mission>()
     
-// Specify properties to ignore (Realm won't persist these)
-    
-//  override static func ignoredProperties() -> [String] {
-//    return []
-//  }
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+class Mission: Object {
+    // MissionとSoundBoxは1対1のリレーション
+    // タイムテーブルのタスク
+    dynamic var title: String = ""
+    dynamic var time_mode: UInt = 0
+    dynamic var finish_at = NSDate()
+    dynamic var number: Int = 0      // ミッションの順番
+    dynamic var lock: Int = 0
+    dynamic var visible: Bool = true // 表示 非表示
+    dynamic var end_sound: Sound?    // 音源を一つ持つ
+
+    override static func primaryKey() -> String? {
+        return "finish_at"
+    }
+}
+
+class SoundBox: Object {
+    // SoundBox と Sound は1対多のリレーション
+    dynamic var title: String = ""
+    dynamic var create_at = NSDate()
+    dynamic var update_at = NSDate()
+    let sound_list = List<Sound>()
+}
+
+class Sound: Object {
+    dynamic var title: String = ""
+    dynamic var trigger_time: Int = 0 // second
+    dynamic var url: String = ""
 }
