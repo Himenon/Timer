4f9aef86218bd4c50cbd1070b68899a9e85b67cc
Kosei Himeno <k.himeno314@gmail.com>
2016-07-17 16:18:43 +0900

    RealmSwiftの追加

---
+++ Timer/AppModel.swift
@@ -0,0 +1,19 @@
+//
+//  AppModel.swift
+//  Timer
+//
+//  Created by Himeno Kosei on 2016/07/17.
+//  Copyright © 2016年 Kosei Himeno. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class AppModel: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
