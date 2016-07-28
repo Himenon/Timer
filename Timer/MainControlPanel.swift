90cd5d2d3093c0ed6260fae13b6de0ddf381ce37
Kosei Himeno <k.himeno314@gmail.com>
2016-07-29 01:13:05 +0900

    表の行選択時の動作用のメソッドを追加

--- Timer/MainControlPanel.swift
+++ Timer/MainControlPanel.swift
@@ -17,29 +17,40 @@ class MainControlPanel: NSViewController {
     var timerVC: NSWindowController?
     var timerViewManaer = TimerViewManager.sharedManager
     
-    let colIDs: [String] = ["MissionNumberCell",
-                            "MissionTitleCell",
-                            "ScheduledFinishTimeCell",
-                            "RealFinishTimeCell",
-                            "TimeReviseCell",
-                            "SoundSettingCell"]
+    let colIDs: [String] = [
+        "MissionNumberColumn",
+        "MissionTitleColumn",
+        "ScheduledFinishTimeColumn",
+        "RealFinishTimeColumn",
+        "TimeReviseColumn",
+        "SoundSettingColumn"
+    ]
+    
+    let celIDs: NSMutableDictionary  = [
+        "MissionNumberColumn": "MissionNumberCell",
+        "MissionTitleColumn": "MissionTitleCell",
+        "ScheduledFinishTimeColumn": "ScheduledFinishTimeCell",
+        "RealFinishTimeColumn": "RealFinishTimeCell",
+        "TimeReviseColumn": "TimeReviseCell",
+        "SoundSettingColumn": "SoundSettingCell",
+        ]
     
     let timeTableData: [NSMutableDictionary] = [
         [
-            "MissionNumberCell": 1,
-            "MissionTitleCell": "my title",
-            "ScheduledFinishTimeCell": "20:10:12",
-            "RealFinishTimeCell": "20:20:15",
-            "TimeReviseCell": "false",
-            "SoundSettingCell": ""
+            "MissionNumberColumn": 1,
+            "MissionTitleColumn": "my title",
+            "ScheduledFinishTimeColumn": "20:10:12",
+            "RealFinishTimeColumn": "20:20:15",
+            "TimeReviseColumn": "false",
+            "SoundSettingColumn": ""
         ],
         [
-            "MissionNumberCell": 2,
-            "MissionTitleCell": "Second Title",
-            "ScheduledFinishTimeCell": "20:16:12",
-            "RealFinishTimeCell": "20:30:15",
-            "TimeReviseCell": "wayway",
-            "SoundSettingCell": ""
+            "MissionNumberColumn": 2,
+            "MissionTitleColumn": "your title",
+            "ScheduledFinishTimeColumn": "20:20:12",
+            "RealFinishTimeColumn": "21:20:15",
+            "TimeReviseColumn": "false",
+            "SoundSettingColumn": ""
         ]
     ]
     
@@ -98,23 +109,17 @@ extension MainControlPanel: NSTableViewDataSource {
         return timeTableData.count
     }
     
-//    func tableView(tableView: NSTableView, setObjectValue object: AnyObject?, forTableColumn tableColumn: NSTableColumn?, row: Int) {
-//        // View-basedになって消えてしまったメソッド。View-basedではもう呼び出されない
-//        print(#function)
-//        print("TableViewのデータを更新します！")
-//        timeTableData[row][(tableColumn?.identifier)!] = object as! String
-//    }
-
+    //    func tableView(tableView: NSTableView, setObjectValue object: AnyObject?, forTableColumn tableColumn: NSTableColumn?, row: Int) {
+    //        // View-basedになって消えてしまったメソッド。View-basedではもう呼び出されない
+    //        print(#function)
+    //        print("TableViewのデータを更新します！")
+    //        timeTableData[row][(tableColumn?.identifier)!] = object as! String
+    //    }
+    
     
 }
 
-extension MainControlPanel: NSTableViewDelegate, NSTextFieldDelegate, NSComboBoxDelegate {
-
-    //
-    override func validateProposedFirstResponder(responder: NSResponder, forEvent event: NSEvent?) -> Bool {
-        return true
-    }
-    
+extension MainControlPanel: NSTableViewDelegate, NSTextFieldDelegate, NSMenuDelegate {
     // NSTableView操作: イベントなどを管理
     // データの取登録
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -122,37 +127,18 @@ extension MainControlPanel: NSTableViewDelegate, NSTextFieldDelegate, NSComboBox
         var text:String = ""
         var cellIdentifier: String = ""
         
-        for (idx, checkCellIdentifier) in colIDs.enumerate() {
+        for (idx, colID) in colIDs.enumerate() {
             if tableColumn == timeTable.tableColumns[idx] {
-                let mymy = timeTableData[row].objectForKey(checkCellIdentifier)
+                let mymy = timeTableData[row].objectForKey(colID)
                 text = String(mymy!)
-                cellIdentifier = checkCellIdentifier
+                cellIdentifier = String(celIDs.objectForKey(colID)!)
             }
             
-            if idx == 4 {
-                if let cell = tableView.makeViewWithIdentifier(cellIdentifier, owner: nil) as? TimeTableCell {
-                    
-                    cell.setValue()
-                    
-                    cell.soundComboBox?.enabled = true
-                    cell.soundComboBox?.editable = true
-                    cell.reviseComboBox?.enabled = true
-                    cell.reviseComboBox?.editable = true
-                    cell.soundComboBox?.setDelegate(self)
-                    cell.reviseComboBox?.setDelegate(self)
-                    return cell
-                }
-            }
-            
-            if idx == 5 {
+            if idx == 4 || idx == 5 {
                 if let cell = tableView.makeViewWithIdentifier(cellIdentifier, owner: nil) as? TimeTableCell {
                     cell.setValue()
-                    cell.soundComboBox?.enabled = true
-                    cell.soundComboBox?.editable = true
-                    cell.reviseComboBox?.enabled = true
-                    cell.reviseComboBox?.editable = true
-                    cell.soundComboBox?.setDelegate(self)
-                    cell.reviseComboBox?.setDelegate(self)
+                    cell.revisePopButton?.target = self
+                    cell.soundPopButton?.target = self
                     return cell
                 }
             }
@@ -164,55 +150,47 @@ extension MainControlPanel: NSTableViewDelegate, NSTextFieldDelegate, NSComboBox
                 return cell
             }
         }
-        
         return nil
     }
     
-    func tableView(tableView: NSTableView, setObjectValue object: AnyObject?, forTableColumn tableColumn: NSTableColumn?, row: Int) {
-        NSLog("\(#function): 値の設定と見込んだ")
-    }
-    
-    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
-        print("\(#function): 値の取得と見込んだ")
-        return true
+    func menuDidClose(menu: NSMenu) {
+        NSLog("\(#function): Selected!")
+        print(menu.itemArray)
     }
     
     // カラムの操作
     func tableView(tableView: NSTableView, didClickTableColumn tableColumn: NSTableColumn) {
+        // Todo: 時刻を選択した場合，時系列を反転させる．
         NSLog(#function)
     }
     
     func tableViewSelectionDidChange(notification: NSNotification) {
-        NSLog("\(#function): セルが選択されました．")
+        NSLog("\(#function): 行が選択されました．")
         let selectedTableView: NSTableView = notification.object as! NSTableView
         let appModelManager = AppModelManager.sharedManager
-        appModelManager.currentSelectedRowIndex = selectedTableView.selectedRow
+        
+        if appModelManager.currentSelectedRowIndex != selectedTableView.selectedRow {
+            timeTableRowSelectedInitalize(selectedTableView, rowNum: appModelManager.currentSelectedRowIndex, enable: false)
+            if selectedTableView.selectedRow >= 0 { // カラムのHeaderを選択した時にエラーが出る対策
+                timeTableRowSelectedInitalize(selectedTableView, rowNum: selectedTableView.selectedRow, enable: true)
+                appModelManager.currentSelectedRowIndex = selectedTableView.selectedRow
+            }
+        }
     }
     
-    
-    func tableViewSelectionIsChanging(notification: NSNotification) {
-        NSLog("\(#function): 選択中の行が変更されました．")
-    }
-    
-    override func controlTextDidBeginEditing(obj: NSNotification) {
-        // 入力開始時に呼び出し
-        NSLog("\(#function): 編集開始")
-    }
-    
-    override func controlTextDidChange(obj: NSNotification) {
-        // 入力中に呼び出し
-        NSLog("\(#function): 編集中")
-    }
-    
-    func control(control: NSControl, textShouldEndEditing fieldEditor: NSText) -> Bool {
-        NSLog("\(#function): Cellの編集終了")
-        print(fieldEditor.string)
-        // [Mac] NSTableView - セル編集終了時のイベントを掴む
-        // http://cocoadays.blogspot.jp/2010/11/mac-nstableview_28.html
-        return true
+    func timeTableRowSelectedInitalize(tableView: NSTableView, rowNum: Int, enable: Bool) {
+        // PopUpMenuのToggle: 選択時のみ有効化
+        let colNum1:Int = tableView.columnWithIdentifier(colIDs[4])
+        let colNum2:Int = tableView.columnWithIdentifier(colIDs[5])
+        for i in [colNum1, colNum2] {
+            let cell = tableView.viewAtColumn(i, row: rowNum, makeIfNecessary: false) as? TimeTableCell
+            cell?.revisePopButton?.enabled = enable
+            cell?.soundPopButton?.enabled =  enable
+        }
     }
     
     override func controlTextDidEndEditing(obj: NSNotification) {
+        print("================================================================================")
         // TextFieldの入力が終了した時に呼び出し
         NSLog("\(#function): TextFieldの入力完了")
         let appModelManager = AppModelManager.sharedManager
@@ -224,18 +202,13 @@ extension MainControlPanel: NSTableViewDelegate, NSTextFieldDelegate, NSComboBox
         }
     }
     
-    func comboBoxSelectionDidChange(notification: NSNotification) {
-        NSLog("\(#function): comboboxのチェンジ！")
-        print(notification)
-    }
-    
 }
 
 
 class TimeTableCell: NSTableCellView {
     // セルの設定
-    @IBOutlet var reviseComboBox: NSComboBox!
-    @IBOutlet var soundComboBox: NSComboBox!
+    @IBOutlet var revisePopButton: NSPopUpButton!
+    @IBOutlet var soundPopButton: NSPopUpButton!
     @IBOutlet var order_numner_label: NSTextField!
     @IBOutlet var text_content: NSTextField!
     @IBOutlet var date_set_field: NSTextField!
@@ -249,15 +222,23 @@ class TimeTableCell: NSTableCellView {
             text_content.editable = true
         }
         
-        if reviseComboBox != nil {
+        if revisePopButton != nil {
+            revisePopButton.removeAllItems()
             for i in 1...10 {
                 // Todo: モデルから編集項目の設定
-                reviseComboBox.insertItemWithObjectValue("Hello \(i)", atIndex: reviseComboBox.numberOfItems)
+                revisePopButton.addItemWithTitle("Hello \(i)")
             }
+            revisePopButton.enabled = false
         }
         
-        if soundComboBox != nil {
+        if soundPopButton != nil {
             // Todo: モデルからサウンドの設定
+            soundPopButton.removeAllItems()
+            for i in 1...10 {
+                // Todo: モデルから編集項目の設定
+                soundPopButton.addItemWithTitle("Sound: \(i)")
+            }
+            soundPopButton.enabled = false
         }
     }
     
